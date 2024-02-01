4d9ce1195dbddf8d53c170c3cecd8393bf3336d4
EAisdead <104033022+EAisdead@users.noreply.github.com>
2024-02-01 17:20:57 +0100

    fixed db

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,14 @@
 const api = {
   item: {
     home: () =>
-      fetch(``).then(
+      fetch(
+        `https://my-json-server.typicode.com/EAisdead/Lootbox-nextjs/db`
+      ).then(
         (res) =>
           res.json() as Promise<{
             items_cat: {
               id: string;
               rate: number;
-              items: {
-                id: number;
-                name: string;
-                image_url: string;
-              }[];
             }[];
           }>
       ),
